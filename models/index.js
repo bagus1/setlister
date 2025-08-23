dc903540fada8ceb37595f82b088da543e368f7f
Bagus1 <bagus@comcast.net>
2025-08-23 16:41:25 -0600

    band relation fix

--- models/index.js
+++ models/index.js
@@ -29,79 +29,87 @@ const SongArtist = require("./SongArtist")(sequelize);
 
 // Define associations
 // User-Band (many-to-many through BandMember)
-User.belongsToMany(Band, { through: BandMember, foreignKey: "userId" });
-Band.belongsToMany(User, { through: BandMember, foreignKey: "bandId" });
+User.belongsToMany(Band, { through: "band_members", foreignKey: "user_id" });
+Band.belongsToMany(User, { through: "band_members", foreignKey: "band_id" });
 
 // Band-Song (many-to-many through BandSong)
-Band.belongsToMany(Song, { through: BandSong, foreignKey: "bandId" });
-Song.belongsToMany(Band, { through: BandSong, foreignKey: "songId" });
+Band.belongsToMany(Song, { through: BandSong, foreignKey: "band_id" });
+Song.belongsToMany(Band, { through: BandSong, foreignKey: "song_id" });
 
 // Song-Artist (many-to-many)
-Song.belongsToMany(Artist, { through: "song_artists", foreignKey: "songId" });
-Artist.belongsToMany(Song, { through: "song_artists", foreignKey: "artistId" });
+Song.belongsToMany(Artist, {
+  through: "song_artists",
+  foreignKey: "song_id",
+  otherKey: "artist_id",
+});
+Artist.belongsToMany(Song, {
+  through: "song_artists",
+  foreignKey: "artist_id",
+  otherKey: "song_id",
+});
 
 // Song-Vocalist (one-to-many)
-Song.belongsTo(Vocalist, { foreignKey: "vocalistId" });
-Vocalist.hasMany(Song, { foreignKey: "vocalistId" });
+Song.belongsTo(Vocalist, { foreignKey: "vocalist_id" });
+Vocalist.hasMany(Song, { foreignKey: "vocalist_id" });
 
 // Medley-Vocalist (one-to-many)
-Medley.belongsTo(Vocalist, { foreignKey: "vocalistId" });
-Vocalist.hasMany(Medley, { foreignKey: "vocalistId" });
+Medley.belongsTo(Vocalist, { foreignKey: "vocalist_id" });
+Vocalist.hasMany(Medley, { foreignKey: "vocalist_id" });
 
 // BandSong junction table associations
-BandSong.belongsTo(Song, { foreignKey: "songId" });
-BandSong.belongsTo(Band, { foreignKey: "bandId" });
-BandSong.belongsTo(GigDocument, { foreignKey: "gigDocumentId" });
-Song.hasMany(BandSong, { foreignKey: "songId" });
-Band.hasMany(BandSong, { foreignKey: "bandId" });
-GigDocument.hasMany(BandSong, { foreignKey: "gigDocumentId" });
+BandSong.belongsTo(Song, { foreignKey: "song_id" });
+BandSong.belongsTo(Band, { foreignKey: "band_id" });
+BandSong.belongsTo(GigDocument, { foreignKey: "gig_document_id" });
+Song.hasMany(BandSong, { foreignKey: "song_id" });
+Band.hasMany(BandSong, { foreignKey: "band_id" });
+GigDocument.hasMany(BandSong, { foreignKey: "gig_document_id" });
 
 // Band-Setlist (one-to-many)
-Band.hasMany(Setlist, { foreignKey: "bandId" });
-Setlist.belongsTo(Band, { foreignKey: "bandId" });
+Band.hasMany(Setlist, { foreignKey: "band_id" });
+Setlist.belongsTo(Band, { foreignKey: "band_id" });
 
 // Setlist-SetlistSet (one-to-many)
-Setlist.hasMany(SetlistSet, { foreignKey: "setlistId" });
-SetlistSet.belongsTo(Setlist, { foreignKey: "setlistId" });
+Setlist.hasMany(SetlistSet, { foreignKey: "setlist_id" });
+SetlistSet.belongsTo(Setlist, { foreignKey: "setlist_id" });
 
 // SetlistSet-SetlistSong (one-to-many)
-SetlistSet.hasMany(SetlistSong, { foreignKey: "setlistSetId" });
-SetlistSong.belongsTo(SetlistSet, { foreignKey: "setlistSetId" });
+SetlistSet.hasMany(SetlistSong, { foreignKey: "setlist_set_id" });
+SetlistSong.belongsTo(SetlistSet, { foreignKey: "setlist_set_id" });
 
 // Song-SetlistSong (one-to-many)
-Song.hasMany(SetlistSong, { foreignKey: "songId" });
-SetlistSong.belongsTo(Song, { foreignKey: "songId" });
+Song.hasMany(SetlistSong, { foreignKey: "song_id" });
+SetlistSong.belongsTo(Song, { foreignKey: "song_id" });
 
 // Medley-Song (many-to-many through MedleySong)
-Medley.belongsToMany(Song, { through: MedleySong, foreignKey: "medleyId" });
-Song.belongsToMany(Medley, { through: MedleySong, foreignKey: "songId" });
+Medley.belongsToMany(Song, { through: MedleySong, foreignKey: "medley_id" });
+Song.belongsToMany(Medley, { through: MedleySong, foreignKey: "song_id" });
 
 // BandInvitation associations
-BandInvitation.belongsTo(Band, { foreignKey: "bandId" });
-Band.hasMany(BandInvitation, { foreignKey: "bandId" });
+BandInvitation.belongsTo(Band, { foreignKey: "band_id" });
+Band.hasMany(BandInvitation, { foreignKey: "band_id" });
 
 // BandInvitation-User (for invitedBy field)
-BandInvitation.belongsTo(User, { as: "Inviter", foreignKey: "invitedBy" });
+BandInvitation.belongsTo(User, { as: "Inviter", foreignKey: "invited_by" });
 User.hasMany(BandInvitation, {
   as: "SentInvitations",
-  foreignKey: "invitedBy",
+  foreignKey: "invited_by",
 });
 
 // PasswordReset associations (if any needed)
 // Currently no associations needed for PasswordReset
 
 // Song-Link (one-to-many)
-Song.hasMany(Link, { foreignKey: "songId" });
-Link.belongsTo(Song, { foreignKey: "songId" });
+Song.hasMany(Link, { foreignKey: "song_id" });
+Link.belongsTo(Song, { foreignKey: "song_id" });
 
 // GigDocument associations
-GigDocument.belongsTo(Song, { foreignKey: "songId" });
-Song.hasMany(GigDocument, { foreignKey: "songId" });
+GigDocument.belongsTo(Song, { foreignKey: "song_id" });
+Song.hasMany(GigDocument, { foreignKey: "song_id" });
 
 // GigDocument creator association
-GigDocument.belongsTo(User, { foreignKey: "createdById", as: "Creator" });
+GigDocument.belongsTo(User, { foreignKey: "created_by_id", as: "Creator" });
 User.hasMany(GigDocument, {
-  foreignKey: "createdById",
+  foreignKey: "created_by_id",
   as: "CreatedGigDocuments",
 });
 

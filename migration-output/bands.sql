37c41f5500b3de19e2520cbd702e80f09ea07a42
Bagus1 <bagus@comcast.net>
2025-08-25 10:06:45 -0600

    Remove migration-output from git tracking - contains sensitive data

--- migration-output/bands.sql
+++
@@ -1,8 +0,0 @@
--- Migration for bands
--- 5 rows
-
-INSERT INTO bands (id, name, description, created_by_id, created_at, updated_at) VALUES ('1', 'Krewe De Groove', '', '1', '2025-07-31 21:17:56.551 +00:00', '2025-07-31 21:17:56.551 +00:00');
-INSERT INTO bands (id, name, description, created_by_id, created_at, updated_at) VALUES ('4', 'Garbo', 'We are the best Bruce Springsteen Cover band on the South Side of Denver.', '6', '2025-08-06 19:35:12.599 +00:00', '2025-08-06 19:35:12.599 +00:00');
-INSERT INTO bands (id, name, description, created_by_id, created_at, updated_at) VALUES ('5', 'The Vagrant Waiters', '', '9', '2025-08-18 02:52:56.338 +00:00', '2025-08-18 02:52:56.338 +00:00');
-INSERT INTO bands (id, name, description, created_by_id, created_at, updated_at) VALUES ('6', 'Jam Thicket', 'Eclectic original groove and surf with provocative song writing, a hard rocking three piece from Charlottesvile, VA', '10', '2025-08-18 14:43:47.698 +00:00', '2025-08-18 14:43:47.698 +00:00');
-INSERT INTO bands (id, name, description, created_by_id, created_at, updated_at) VALUES ('7', 'Captain Crip; The Rolling Rocker', 'Solo project from a jam band piano/guitar player', '11', '2025-08-19 15:34:46.202 +00:00', '2025-08-19 15:34:46.202 +00:00');

4a98957664e362bcfbb4955a4b147cba4c30d011
Bagus1 <bagus@comcast.net>
2025-10-24 15:36:38 -0600

    path

--- routes/bands.js
+++ routes/bands.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const { prisma } = require("../lib/prisma");
+const path = require("path");
 const {
   sendBandInvitation,
   sendBandInvitationNotification,
